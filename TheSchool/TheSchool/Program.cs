f9c0c26513227d32a257fdf3794d19dc2b59c42c
Ana <ana.pinzari@icloud.com>
2022-02-17 21:18:33 +0200

    Initial commit

---
+++ TheSchool/TheSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
